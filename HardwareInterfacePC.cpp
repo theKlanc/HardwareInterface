b4fde1a6787221bc5e104f13006a897446b40017
Lluís Trilla <clank201@gmail.com>
2020-03-11 22:14:35 +0100

    Small header fix

--- HardwareInterfacePC.cpp
+++ HardwareInterfacePC.cpp
@@ -12,10 +12,12 @@
 #include "HardwareInterface.hpp"
 #include <thread>
 #include <filesystem>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
+
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+
 #include <fstream>
 #include <functional>
 
